d6ef93fbd7f3ec09116a2b392bfe5cafd8a36d67
Heru Handika <herubiolog@gmail.com>
2020-12-23 09:09:55 -0600

    Fix errors for changing variable names in write files.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,24 @@
 //! Support multiline.
 mod counter;
 
+use clap::{App, Arg};
 use counter::fasta as fast;
 
 use std::string::String;
 
 fn main() {
-    let f = String::from("data/b_chrysocomus.fasta");
+    let args = App::new("GC-Counter")
+        .version("0.0.1")
+        .about("Quickly count gc content from a fasta file.")
+        .arg(Arg::with_name("input")
+            .help("Fasta file to analyze.")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+
+    let input = args.value_of("input").unwrap();
+    let f = String::from(input);
     let seq = fast::read_file(&f);
-    fast::write_results(&seq);
+    fast::print_results(&seq);
 }
 
