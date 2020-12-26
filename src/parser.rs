297adc56b5d6e6a92ca56e35b4ddd2764da04b6b
Heru Handika <herubiolog@gmail.com>
2020-12-25 18:34:09 -0600

    Add function to parse fastaq file.

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,28 @@
 pub parse {
-    pub fn parse_fastaq() {
-        let f = File::open()
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::io::BufReader;
+
+    pub fn parse_fastaq(input: &String; output: &String) {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+
+        let output_file = format!("{}{}", output, ".csv");
+        let file = File::create(&output_file).unwrap();
+        let mut file = LineWriter::new(file);
+
+        // writeln!(file, "Read,GC-Content, GC-Ratio").unwrap();
+        let mut read: i32 = 0;
+        for l_ in reader.lines() {
+            let line = l_.unwrap();
+            if line.starts_with("@") {
+                read += 1
+                print!("Read {}", read);
+            } 
+            while !line.starts_with("+") {
+                let seq = line;
+                println!("{}", seq);
+            }
+        }
     }
 }
